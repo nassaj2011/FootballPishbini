ec63a1a6693a6c12f6fb44ec8fc615b84f783639
Naser <naser.sajady@gmail.com>
2026-07-01 12:53:32 +0330

    user mapping database clear

--- database.py
+++ database.py
@@ -2,46 +2,23 @@ import os
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-# 🎯 قدم اول: دیکشنری تبدیل نام کاربری به نام محترمانه و تابع مبدل
-USER_MAPPING = {
-    USER_MAPPING = {
-    "Hadi": "آقا هادی لطفی",
-    "AmirAKS9": "امیر آقا",
-    "Nima": "آقا نیما",
-    "naser": "آقا ناصر",
-    "gemany": "آقا ساجد",
-    "Sana": "آقا سعید",
-    "Hamid": "آقا حمید",
-    "alisaj": "علی آقا سجادی",
-    "Alims": "علی آقا متولیان",
-    "مسعود": "مسعود آقا",
-    
-    # 🌟 تمام حالت‌های احتمالی برای اکانت آقا نادر اضافه شد
-    "ایران_رویایی": "آقا نادر",
-    "ایران رویایی": "آقا نادر",
-    "ایران رويایی": "آقا نادر",  # با یِ عربی
-    "ایران رويايي": "آقا نادر",  # با یِ عربی دوبل
-    
-    "Hadisajadi": "هادی آقا متولیان",
-    "Amir_Rainbow": "امیر آقا عباسی"
-}
-}
-
-def get_persian_name(username):
-    return USER_MAPPING.get(username, username)
 
 # ==========================================
 # تنظیمات اصلی دیتابیس
 # ==========================================
 
+
 if not os.path.exists("data"):
     os.makedirs("data")
 
+
 SQLALCHEMY_DATABASE_URL = "sqlite:///./data/football.db"
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
 
 class User(Base):
     __tablename__ = "users"
@@ -52,6 +29,7 @@ class User(Base):
     score = Column(Integer, default=0)
     last_login = Column(String, default="هرگز")
     previous_rank = Column(Integer, default=1)
+
 
 class Match(Base):
     __tablename__ = "matches"
@@ -67,6 +45,7 @@ class Match(Base):
     actual_home_goals = Column(Integer, nullable=True)
     actual_away_goals = Column(Integer, nullable=True)
 
+
 class Prediction(Base):
     __tablename__ = "predictions"
     id = Column(Integer, primary_key=True, index=True)
@@ -75,6 +54,7 @@ class Prediction(Base):
     predicted_home_goals = Column(Integer)
     predicted_away_goals = Column(Integer)
     submit_time = Column(String, default="نامشخص") # 🌟 این خط جا مانده بود!
+
 
 class AuditLog(Base):
     __tablename__ = "audit_logs"
@@ -86,9 +66,12 @@ class AuditLog(Base):
     user_agent = Column(String)
     timestamp = Column(String)
 
+
 class SystemSetting(Base):
     __tablename__ = "system_settings"
     id = Column(Integer, primary_key=True, index=True)
     key = Column(String, unique=True, index=True)
     value = Column(String)
+
+
 
